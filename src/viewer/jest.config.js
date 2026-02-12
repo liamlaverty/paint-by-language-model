a6951da1bb6f32801a70b0443306ba3b6fd0acbb
LLaverty <liamlaverty@gmail.com>
2026-02-12 14:56:51 +0000

    build(pnpm-tools): update to include test libraries for ocmponents

--- src/viewer/jest.config.js
+++ src/viewer/jest.config.js
@@ -6,7 +6,14 @@ const config = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx', '**/*.test.ts', '**/*.test.tsx'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+    }],
+  },
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
